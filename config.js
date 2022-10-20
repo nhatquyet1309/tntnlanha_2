1f34a9657bbb9a9477838b31f0812b6ad28999bf
nhatquyet1309 <116267747+nhatquyet1309@users.noreply.github.com>
2022-10-20 21:11:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Tập thể Nam TNTN xin chàoooo ! 🖐🖐🖐',
+    introDesc: `Thêm một món quà nữa chúng mình dành tặng các cậu`,
+    btnIntro: 'Nhận quà thuii',
+    title: 'Các cậu lả những người tuyệt vời nhất thế gian ? 😙',
+    desc: 'Chờ một chút trong lúc chúng mình chuẩn bị quà nhé :> ',
+    btnYes: 'Yêu ơi là yêuu 💖💖💖',
+    btnNo: '💔 Tớ ngại quá ! 💔💦 ',
+    question:'Trước khi nhận quà các bạn muốn nhắn nhủ gì với chúng mình không nào 😊😊',
+    reasonPlaceholder: 'Viết vào đây ne',
+    btnReply: '🎁 Nhận quà ! 💌',
+    reply: 'Nam TNTN cute nhất hệ mặt trờii 😊😊',
+    mess: 'Hihi chúng mình biết mà 🥰. Nhận quà ne  😘😘',
+    messDesc: '💬💬 Vẻ đẹp của Trái đất là những bông hoa. Vẻ đẹp của cuộc sống là nụ cười của người phụ nữ. Vì vậy, hãy nở hoa, lan tỏa hạnh phúc và vẻ đẹp của cậu. Chúc cậu ngày lễ 20/10 ý nghĩa và mãi yêu mái nhà TNTN nhe💙.  💬💬',
+    btnAccept: 'HAPPY VIETNAM WOMENS DAY 💖',
+    messLink: 'https://fb.watch/gh363TGOo_/'
+}
